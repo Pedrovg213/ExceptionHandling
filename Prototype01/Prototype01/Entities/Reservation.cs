644f73f786226924778cc3d54d40d046e5a51e23
Pedrovg213 <pedrovg213@gmail.com>
2022-07-05 15:33:04 -0300

    Prototype01: Personalized Exception Handling
    - A bad solution of personalized excess handling. Using several 'if-else' to force the user enter with the right data.

---
+++ Prototype01/Prototype01/Entities/Reservation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype01.Entities {
+   internal class Reservation {
+
+      public int RoomNumber {
+         get; set;
+      }
+      public DateTime CheckIn {
+         get; set;
+      }
+      public DateTime CheckOut {
+         get; set;
+      }
+
+
+      public Reservation( ) {
+      }
+      public Reservation( int _roomNumber , DateTime _checkIn , DateTime _checkOut ) {
+
+         RoomNumber = _roomNumber;
+         CheckIn = _checkIn;
+         CheckOut = _checkOut;
+      }
+
+
+      public void UpdateDates( DateTime _checkIn , DateTime _checkOut ) {
+         CheckIn = _checkIn;
+         CheckOut = _checkOut;
+      }
+      public int Duration( ) {
+
+         TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+         return ( int )duration.TotalDays;
+      }
+
+
+      public override string ToString( ) {
+
+         return $"Room: {RoomNumber}, " +
+            $"CheckIn: {CheckIn.ToString( "dd/MM/yyyy" )}, " +
+            $"CheckOut: {CheckOut.ToString( "dd/MM/yyyy" )}, " +
+            $"Duration: {Duration()} nights.";
+      }
+   }
+}
--- Prototype01/Prototype01/Program.cs
+++ Prototype01/Prototype01/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Prototype01.Entities;
 
 namespace Prototype01 {
    internal class Program {
       static void Main( string[ ] args ) {
+
+         // Inputs
+         // room number
+         Console.Write( "Room Number: " );
+         int roomNumber = int.Parse(Console.ReadLine());
+         // check in
+         Console.Write( "Check in date (dd/mm/yyyy): " );
+         DateTime checkIn = DateTime.Parse(Console.ReadLine());
+         // check out
+         Console.Write( "Check out date (dd/mm/yyyy): " );
+         DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+         if ( checkOut <= checkIn ) {
+            Console.WriteLine( "Reservation Error: Check date out must be after check in date." );
+         } else {
+            // Instantiation
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            Console.WriteLine( reservation );
+
+            // Update
+            Console.WriteLine( "Enter the data to uptade reservation:" );
+            // check in
+            Console.Write( "Check in date (dd/mm/yyyy): " );
+            checkIn = DateTime.Parse( Console.ReadLine() );
+            // check out
+            Console.Write( "Check out date (dd/mm/yyyy): " );
+            checkOut = DateTime.Parse( Console.ReadLine() );
+
+            DateTime now = DateTime.Now;
+            if ( checkIn < now || checkOut < now ) {
+               Console.WriteLine( "Reservation Error: Reservation dates to uptades must be future dates." );
+            } else if ( checkOut <= checkIn ) {
+               Console.WriteLine( "Reservation Error: Check date out must be after check in date." );
+            } else {
+               reservation.UpdateDates( checkIn , checkOut );
+               Console.WriteLine( reservation );
+            }
+         }
       }
    }
 }
